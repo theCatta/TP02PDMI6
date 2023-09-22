5689d23f7c12af271f1bd5f3ac95fcb6fdceab79
Guilherme Ferreira <guilherme.fs.db@gmail.com>
2023-09-22 19:04:08 -0300

    Add files via upload

---
+++ TarefasApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace TarefasApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
